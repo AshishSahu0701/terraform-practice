f7ca5b199a5d1c96dfdb2f09592bebb7b0938a11
Ashish Sahu <sahu.ashish0701@gmail.com>
2026-08-03 20:42:12 +0530

    new rg added

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "RG1" {
   name = "rg-tata"
   location = "central india"
 }
+
+resource "azurerm_resource_group" "RG2" {
+  name = "rg-ashish"
+  location = "central india"
+}
